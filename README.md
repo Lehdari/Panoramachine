9d8f1ffe6cdf16b056f40df568b06fde0d560001
Lehdari <miika.lehtimaki@gmail.com>
2021-09-20 22:16:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Image Demorphing
+================
+
+Demorph images
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Project: image_demorphing
+// File: main.cpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+int main(void) {
+    return 0;
+}
