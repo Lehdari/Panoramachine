bfef0ba4f5d08d69c5a94f09b4c59f0af4b5a741
Lehdari <miika.lehtimaki@gmail.com>
2021-10-09 22:38:05 +0300

    CorrectionAlgorithm2: Implement feature mining

--- src/CorrectionAlgorithm2.cpp
+++ src/CorrectionAlgorithm2.cpp
@@ -13,13 +13,43 @@
 #include <opencv2/highgui.hpp>
 #include <fftw3.h>
 #include <mutex>
+#include <random>
+#include <iostream> // TODO temp
+#include <KdTree.hpp>
 
 
 struct Feature {
+    static constexpr int fsa = 32; // feature axial size
+    static constexpr int fsr = 128; // feature radial size
+    static constexpr float frf = 1.0f; // first feature radius
+    static constexpr float frm = 1.189207115f; // feature radius multiplier
+#if 0
     cv::Mat polar;
     cv::Mat amplitude;
     cv::Mat phase;
+#endif
+    Eigen::Matrix<float, fsa*6, fsr/2>  flattenedFft;
+    Eigen::Matrix<float, 2, fsr/2>      intermediate;
+    Vec2f                               projected;
+
+    std::vector<Vec2f>                  targets;
+    std::vector<Vec2f>                  antiTargets;
+
+    using PMatrix1 = Eigen::Matrix<float, 2, fsa*6>;
+    using PMatrix2 = Eigen::Matrix<float, fsr/2, 1>;
+
+    static PMatrix1 projectionMatrix1;
+    static PMatrix2 projectionMatrix2;
+
+    void update(void)
+    {
+        intermediate = Feature::projectionMatrix1 * flattenedFft;
+        projected = intermediate * Feature::projectionMatrix2;
+    }
 };
+
+Eigen::Matrix<float, 2, Feature::fsa*6> Feature::projectionMatrix1 = Eigen::Matrix<float, 2, Feature::fsa*6>::Random();
+Eigen::Matrix<float, Feature::fsr/2, 1> Feature::projectionMatrix2 = Eigen::Matrix<float, Feature::fsr/2, 1>::Random();
 
 class FFTWPlan {
 public:
@@ -57,79 +87,215 @@ private:
 std::mutex FFTWPlan::_mutex;
 
 
-void fourierTransform(const Vec3f* f, Vec3f* va, Vec3f* vp, int n)
+void fourierTransform(const Vec3f* f, std::complex<double> fftRow[Feature::fsr/2][3])
 {
     static std::vector<FFTWPlan> plans = [&](){
         std::vector<FFTWPlan> plans;
         plans.reserve(omp_get_num_threads());
         for (int i=0; i<omp_get_num_threads(); ++i)
-            plans.emplace_back(n);
+            plans.emplace_back(Feature::fsr);
         return plans;
     }();
 
     auto& p = plans[omp_get_thread_num()];
     for (int c=0; c<3; ++c) {
-        for (int i=0; i<n; ++i) {
+        for (int i=0; i<Feature::fsr; ++i) {
             p.in[i][0] = f[i](c);
             p.in[i][1] = 0.0;
         }
 
         p.execute();
 
-        for (int i=0; i<n; ++i) {
-            va[i](c) = std::log(std::sqrt(p.out[i][0]*p.out[i][0] + p.out[i][1]*p.out[i][1]));
-            vp[i](c) = 0.5f+0.5f*std::atan2(p.out[i][1], p.out[i][0]);
+        for (int i=0; i<Feature::fsr/2; ++i) {
+            fftRow[i][c] = *reinterpret_cast<std::complex<double>*>(&p.out[i]);
         }
     }
 }
 
-void computeFeature(const cv::Mat& img, int x, int y, Feature& feature)
+inline __attribute__((always_inline)) double phaseDiff(
+    const std::complex<double>& a, const std::complex<double>& b)
 {
-    constexpr int fsa = 32; // feature axial size
-    constexpr int fsr = 128; // feature radial size
-    constexpr float frf = 1.0f; // first feature radius
-    constexpr float frm = 1.189207115f; // feature radius multiplier
+    double diff = std::atan2(b.imag(), b.real()) - std::atan2(a.imag(), a.real());
+    if (diff < -M_PI)
+        diff += 2.0*M_PI;
+    if (diff > M_PI)
+        diff -= 2.0*M_PI;
+    return diff;
+}
 
-    feature.polar = cv::Mat(fsa, fsr, CV_32FC3);
-    feature.amplitude = cv::Mat(fsa, fsr, CV_32FC3);
-    feature.phase = cv::Mat(fsa, fsr, CV_32FC3);
+Feature computeFeature(const cv::Mat& img, int x, int y)
+{
+    Feature feature;
+    cv::Mat polar = cv::Mat(Feature::fsa, Feature::fsr, CV_32FC3);
+#if 0
+    feature.amplitude = cv::Mat(Feature::fsa, Feature::fsr/2, CV_32FC3);
+    feature.phase = cv::Mat(Feature::fsa, Feature::fsr/2, CV_32FC3);
+#endif
 
-    float r = frf;
-    for (int j=0; j<fsa; ++j) {
-        auto* f = feature.polar.ptr<Vec3f>(j);
-        for (int i=0; i<fsr; ++i) {
-            float angle = 2.0f*M_PI*(i/(float)fsr);
+    std::complex<double> fft[Feature::fsa][Feature::fsr/2][3];
+
+    float r = Feature::frf;
+    for (int j=0; j<Feature::fsa; ++j) {
+        auto* f = polar.ptr<Vec3f>(j);
+        for (int i=0; i<Feature::fsr; ++i) {
+            float angle = 2.0f*M_PI*(i/(float)Feature::fsr);
             f[i] = sampleMatCubic(img, Vec2f(x+r*std::cos(angle), y+r*std::sin(angle)));
         }
 
-        fourierTransform(f, feature.amplitude.ptr<Vec3f>(j), feature.phase.ptr<Vec3f>(j), fsr);
-        r *= frm;
+        fourierTransform(f, fft[j]);
+
+        r *= Feature::frm;
     }
+
+    for (int j=0; j<Feature::fsa; ++j) {
+#if 0
+        auto* pa = feature.amplitude.ptr<Vec3f>(j);
+        auto* pp = feature.phase.ptr<Vec3f>(j);
+#endif
+        for (int i=0; i<Feature::fsr/2; ++i) {
+#if 0
+            pa[i] = Vec3f(
+                std::log(std::abs(fft[j][i][0])),
+                std::log(std::abs(fft[j][i][1])),
+                std::log(std::abs(fft[j][i][2])));
+            if (i < Feature::fsr/2-1) {
+                pp[i] = Vec3f(
+                    0.5f + M_1_PI * phaseDiff(fft[j][i][0], fft[j][i + 1][0]),
+                    0.5f + M_1_PI * phaseDiff(fft[j][i][1], fft[j][i + 1][1]),
+                    0.5f + M_1_PI * phaseDiff(fft[j][i][2], fft[j][i + 1][2]));
+            }
+            else {
+                pp[i] = Vec3f(0.0f, 0.0f, 0.0f);
+            }
+#endif
+
+            for (int c=0; c<3; ++c) {
+                feature.flattenedFft(j+c*Feature::fsa, i) = std::abs(fft[j][i][c]);
+                if (i < Feature::fsr/2-1)
+                    feature.flattenedFft(j+(c+3)*Feature::fsa, i) =
+                        phaseDiff(fft[j][i][0], fft[j][i+1][0]);
+                else
+                    feature.flattenedFft(j+(c+3)*Feature::fsa, i) = 0.0f;
+            }
+        }
+    }
+
+    feature.update();
+
+    //printf("projected: [ %0.5f %0.5f ]\n", feature.projected(0), feature.projected(1));
+
+    return feature;
 }
 
 void clickCallBack(int event, int x, int y, int flags, void* imagePtr)
 {
     auto& img = *static_cast<const cv::Mat*>(imagePtr);
 
-    Feature feature;
-    computeFeature(img, x, y, feature);
-
+    Feature feature = computeFeature(img, x, y);
+#if 0
     cv::imshow("feature.polar", feature.polar);
     cv::imshow("feature.amplitude", feature.amplitude);
     cv::imshow("feature.phase", feature.phase);
-    cv::waitKey(20);
+#endif
 }
 
 cv::Mat createCorrection2(const cv::Mat& source, const cv::Mat& target)
 {
     cv::Mat corr = source.clone() * 0.0f;
-/*
+#if 0
     cv::namedWindow("source");
     cv::setMouseCallback("source", clickCallBack, (void*)&source);
 
     cv::imshow("source", source);
     cv::waitKey();
-    */
+
+#elif 1
+
+    std::default_random_engine rnd(1507715517);
+
+    cv::Mat projectedFeatures = source.clone() * 0.0f;
+
+    constexpr int nFeatures = 10000;
+    std::vector<Feature> features;
+    for (int i=0; i<nFeatures; ++i) {
+        int sx = rnd() % source.cols;
+        int sy = rnd() % source.rows;
+
+        features.emplace_back(computeFeature(source, sx, sy));
+    }
+
+    for (int o=0; o<10000; ++o) {
+        printf("o: %d\n", o);
+        projectedFeatures *= 0.0f;
+
+        for (auto& feature: features) {
+            int px = source.cols / 2 + feature.projected(0);
+            int py = source.rows / 2 + feature.projected(1);
+            if (px < 0 || px >= source.cols || py < 0 || py >= source.rows)
+                continue;
+
+            projectedFeatures.at<Vec3f>(py, px) = Vec3f(1.0f, 1.0f, 1.0f);
+        }
+
+        KdTree<Vec2f> tree;
+        for (auto& feature : features) {
+            feature.update();
+            feature.targets.clear();
+            feature.antiTargets.clear();
+            tree.addPoint(feature.projected);
+        }
+        tree.build();
+
+        Vec2f globalCentroid(0.0f, 0.0f);
+        for (auto& feature : features) {
+            globalCentroid += feature.projected;
+        }
+        globalCentroid /= nFeatures;
+
+        constexpr int neighbourhoodSize = 10;
+        std::vector<const Vec2f*> nearest;
+        for (auto& feature : features) {
+            tree.getKNearest(feature.projected, neighbourhoodSize, nearest);
+
+            Vec2f centroid(0.0f, 0.0f);
+            for (auto& p : nearest)
+                centroid += *p;
+            centroid /= neighbourhoodSize;
+
+            feature.targets.push_back(centroid);
+            feature.targets.emplace_back(0.0f, 0.0f);
+            feature.antiTargets.emplace_back(globalCentroid);
+        }
+
+        // Gradient descent
+        constexpr double gdRate = 1.0e-5 / nFeatures;
+
+        Feature::PMatrix1 gm1 = Feature::PMatrix1::Zero();
+        Feature::PMatrix2 gm2 = Feature::PMatrix2::Zero();
+        for (auto& feature: features) {
+            for (auto& fTarget : feature.targets) {
+                gm2 += feature.intermediate.transpose() * (feature.projected - fTarget);
+                gm1 += ((feature.projected - fTarget) *
+                    (feature.flattenedFft * Feature::projectionMatrix2).transpose());
+            }
+            for (auto& fAntiTarget : feature.antiTargets) {
+                gm2 += feature.intermediate.transpose() * (fAntiTarget - feature.projected);
+                gm1 += (fAntiTarget - feature.projected) *
+                    (feature.flattenedFft * Feature::projectionMatrix2).transpose();
+            }
+        }
+
+        Feature::projectionMatrix1 -= gm1 * gdRate;
+        Feature::projectionMatrix2 -= gm2 * gdRate;
+
+        cv::imshow("projectedFeatures", projectedFeatures);
+        cv::waitKey(20);
+    }
+
+    cv::imshow("projectedFeatures", projectedFeatures);
+    cv::waitKey();
+
+#elif
 
     //std::vector<Feature> features(source.rows * source.cols);
 
@@ -164,6 +330,6 @@ cv::Mat createCorrection2(const cv::Mat& source, const cv::Mat& target)
     cv::imshow("fImage", fImage);
     cv::waitKey();
     cv::imwrite("fImage.exr", fImage);
-
+#endif
     return corr;
 }
