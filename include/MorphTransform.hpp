2e07522ea6a12f18abf9e06406a970a7912f3e69
Lehdari <miika.lehtimaki@gmail.com>
2021-11-13 00:06:41 +0200

    Make MorphTransform agnostic to scalar type

--- include/MorphTransform.hpp
+++ include/MorphTransform.hpp
@@ -12,34 +12,107 @@
 #define IMAGE_DEMORPHING_MORPHTRANSFORM_HPP
 
 
-#include "MathTypes.hpp"
+#include <Eigen/Dense>
+#include <random>
 
 
+template <typename T_Scalar>
 class MorphTransform {
 public:
-    MorphTransform(
-        const Vec2f& position,
-        float distance = 0.0f,
-        float rotation = 0.0f,
-        float scale = 1.0f,
-        const Vec2f& translation = Vec2f(0.0f, 0.0f));
+    using Vec2 = Eigen::Matrix<T_Scalar, 2, 1>;
+    using Vec3 = Eigen::Matrix<T_Scalar, 3, 1>;
+    using Mat3 = Eigen::Matrix<T_Scalar, 3, 3>;
 
-    Vec2f operator*(const Vec2f& v);
+    MorphTransform(
+        const Vec2& position,
+        T_Scalar distance = 0.0,
+        T_Scalar rotation = 0.0,
+        T_Scalar scale = 1.0,
+        const Vec2& translation = Vec2(0.0f, 0.0f));
+
+    Vec2 operator*(const Vec2& v);
 
     static MorphTransform randomTransform(
-        const Vec2f& maxPosition,
-        float minDistance, float maxDistance,
-        float maxRotation,
-        float minScale, float maxScale,
-        const Vec2f& maxTranslation);
+        const Vec2& maxPosition,
+        T_Scalar minDistance, T_Scalar maxDistance,
+        T_Scalar maxRotation,
+        T_Scalar minScale, T_Scalar maxScale,
+        const Vec2& maxTranslation);
 
 private:
-    Mat3f   _mat;
-    Vec2f   _p;
-    float   _d2;
+    Mat3    _mat;
+    Vec2    _p;
+    T_Scalar   _d2;
 
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
+
+
+#define RND ((rnd()%1000001) * 0.000001)
+
+
+template <typename T_Scalar>
+MorphTransform<T_Scalar>::MorphTransform(
+    const Vec2& position,
+    T_Scalar distance,
+    T_Scalar rotation,
+    T_Scalar scale,
+    const Vec2& translation
+) :
+    _p  (position),
+    _d2 (distance*distance/9.0f)
+{
+    Mat3 m1, m2, t, r, s;
+    m1  <<  1.0f,   0.0f,   -position(0),
+        0.0f,   1.0f,   -position(1),
+        0.0f,   0.0f,   1.0f;
+    m2  <<  1.0f,   0.0f,   position(0),
+        0.0f,   1.0f,   position(1),
+        0.0f,   0.0f,   1.0f;
+    t   <<  1.0f,   0.0f,   translation(0),
+        0.0f,   1.0f,   translation(1),
+        0.0f,   0.0f,   1.0f;
+    r   <<  std::cos(rotation), -std::sin(rotation),    0.0f,
+        std::sin(rotation), std::cos(rotation),     0.0f,
+        0.0f,               0.0f,                   1.0f;
+    s   <<  scale,  0.0f,   0.0f,
+        0.0f,   scale,  0.0f,
+        0.0f,   0.0f,   1.0f;
+
+    _mat = (m2 * t * r * s * m1).inverse();
+}
+
+template <typename T_Scalar>
+typename MorphTransform<T_Scalar>::Vec2 MorphTransform<T_Scalar>::operator*(const Vec2& v)
+{
+    Vec3 vv;
+    vv << v, 1.0f;
+    Vec2 v2 = (_mat * vv).template block<2,1>(0,0);
+
+    // transformation filtering
+    T_Scalar f = std::exp( -0.5f * ((v-_p).squaredNorm() / _d2) );
+
+    return v + f*(v2-v);
+}
+
+template <typename T_Scalar>
+MorphTransform<T_Scalar> MorphTransform<T_Scalar>::randomTransform(
+    const Vec2& maxPosition,
+    T_Scalar minDistance, T_Scalar maxDistance,
+    T_Scalar maxRotation,
+    T_Scalar minScale, T_Scalar maxScale,
+    const Vec2& maxTranslation)
+{
+    static std::default_random_engine rnd(1507715517);
+    return MorphTransform(Vec2(RND*maxPosition(0), RND*maxPosition(1)),
+        minDistance + RND*(maxDistance-minDistance),
+        maxRotation - 2.0*RND*maxRotation,
+        minScale + RND*(maxScale-minScale),
+        maxTranslation - 2.0*RND*maxTranslation);
+}
+
+
+#undef RND
 
 
 #endif //IMAGE_DEMORPHING_MORPHTRANSFORM_HPP
