f8b4c247e598c194ba5a7e5bb29c3e72b3935850
Lehdari <miika.lehtimaki@gmail.com>
2021-11-17 02:59:41 +0200

    Add Feature class

---
+++ include/Feature.hpp
@@ -0,0 +1,35 @@
+//
+// Project: image_demorphing
+// File: Feature.hpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#ifndef IMAGE_DEMORPHING_FEATURE_HPP
+#define IMAGE_DEMORPHING_FEATURE_HPP
+
+
+#include <opencv2/core/mat.hpp>
+
+#include "MathTypes.hpp"
+
+
+struct Feature {
+    static constexpr int fsa = 32; // feature axial size
+    static constexpr int fsr = 128; // feature radial size
+    static constexpr float frm = 1.1387886347566f; // feature radius multiplier
+
+    Eigen::Matrix<float, fsa*3, fsr> polar;
+    Eigen::Matrix<float, fsa*3, fsr> polarDiff;
+
+    Feature(const cv::Mat& img, const Vec2f& p, float firstRadius);
+};
+
+
+void visualizeFeature(Feature& feature, const std::string& windowName);
+
+
+#endif //IMAGE_DEMORPHING_FEATURE_HPP
---
+++ src/Feature.cpp
@@ -0,0 +1,41 @@
+//
+// Project: image_demorphing
+// File: Feature.cpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include "Feature.hpp"
+#include "Utils.hpp"
+
+#include <opencv2/highgui.hpp>
+
+
+Feature::Feature(const cv::Mat& img, const Vec2f& p, float firstRadius)
+{
+    for (int i=0; i<Feature::fsr; ++i) {
+        float angle = 2.0f*M_PI*(i/(float)Feature::fsr);
+        float r = firstRadius;
+        Vec2f dir(std::cos(angle), std::sin(angle));
+        for (int j=0; j<Feature::fsa; ++j) {
+            polar.block<3,1>(j*3,i) = sampleMatCubic<Vec3f>(img, p+dir*r);
+            r *= Feature::frm;
+        }
+    }
+
+    for (int i=0; i<Feature::fsr; ++i) {
+        polarDiff.block<Feature::fsa*3, 1>(0, i) =
+            polar.block<Feature::fsa*3, 1>(0, (i+1)%Feature::fsr) -
+                polar.block<Feature::fsa*3, 1>(0, i);
+    }
+}
+
+void visualizeFeature(Feature& feature, const std::string& windowName)
+{
+    cv::Mat featureImg(Feature::fsr, Feature::fsa, CV_32FC3);
+    featureImg.data = reinterpret_cast<unsigned char*>(feature.polar.data());
+    cv::imshow(windowName, featureImg);
+}
