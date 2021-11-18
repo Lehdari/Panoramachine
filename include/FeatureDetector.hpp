ba6f31f3dd7b74b04b634f65392e2cf0b63328b4
Lehdari <miika.lehtimaki@gmail.com>
2021-11-18 03:15:51 +0200

    Add FeatureDetector class

---
+++ include/FeatureDetector.hpp
@@ -0,0 +1,56 @@
+//
+// Project: image_demorphing
+// File: FeatureDetector.hpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#ifndef IMAGE_DEMORPHING_FEATUREDETECTOR_HPP
+#define IMAGE_DEMORPHING_FEATUREDETECTOR_HPP
+
+
+#include "TrainFeatureDetector.hpp"
+#include "NeuralNetwork.hpp"
+
+
+class FeatureDetector {
+public:
+    FeatureDetector();
+
+    double trainBatch(const TrainingBatch& batch);
+
+
+private:
+    using Layer1 = LayerMerge<float, ActivationReLU, OptimizerAdam, Feature::fsa*6, Feature::fsr, 64>;
+    using Layer2 = LayerMerge<float, ActivationReLU, OptimizerAdam, 64, Feature::fsr/2, 64>;
+    using Layer3 = LayerMerge<float, ActivationReLU, OptimizerAdam, 64, Feature::fsr/4, 64>;
+    using Layer4 = LayerMerge<float, ActivationReLU, OptimizerAdam, 64, Feature::fsr/8, 64>;
+    using Layer5 = LayerMerge<float, ActivationReLU, OptimizerAdam, 64, Feature::fsr/16, 64>;
+    using Layer6 = LayerMerge<float, ActivationReLU, OptimizerAdam, 64, Feature::fsr/32, 64>;
+    using Layer7 = LayerMerge<float, ActivationReLU, OptimizerAdam, 64, Feature::fsr/64, 64>;
+    using Layer8 = LayerDense<float, ActivationReLU, OptimizerAdam, 64, 32>;
+    using Layer9 = LayerDense<float, ActivationReLU, OptimizerAdam, 32, 16>;
+    using Layer10 = LayerDense<float, ActivationTanh, OptimizerAdam, 16, 8>;
+    using Layer11 = LayerDense<float, ActivationTanh, OptimizerAdam, 8, 1>;
+
+    Layer1  _layer1a, _layer1b;
+    Layer2  _layer2a, _layer2b;
+    Layer3  _layer3a, _layer3b;
+    Layer4  _layer4a, _layer4b;
+    Layer5  _layer5a, _layer5b;
+    Layer6  _layer6a, _layer6b;
+    Layer7  _layer7a, _layer7b;
+    Layer8  _layer8a, _layer8b;
+    Layer9  _layer9a, _layer9b;
+    Layer10 _layer10a, _layer10b;
+    Layer11 _layer11;
+
+
+    float trainingPass(const Feature& f1, const Feature& f2, float targetSimilarity);
+};
+
+
+#endif //IMAGE_DEMORPHING_FEATUREDETECTOR_HPP
---
+++ src/FeatureDetector.cpp
@@ -0,0 +1,95 @@
+//
+// Project: image_demorphing
+// File: FeatureDetector.cpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include "FeatureDetector.hpp"
+
+
+FeatureDetector::FeatureDetector() :
+    _layer1a(0.01f, ActivationReLU(0.01f)), _layer1b(ActivationReLU(0.01f), _layer1a.getOptimizer()),
+    _layer2a(0.01f, ActivationReLU(0.01f)), _layer2b(ActivationReLU(0.01f), _layer2a.getOptimizer()),
+    _layer3a(0.01f, ActivationReLU(0.01f)), _layer3b(ActivationReLU(0.01f), _layer3a.getOptimizer()),
+    _layer4a(0.01f, ActivationReLU(0.01f)), _layer4b(ActivationReLU(0.01f), _layer4a.getOptimizer()),
+    _layer5a(0.01f, ActivationReLU(0.01f)), _layer5b(ActivationReLU(0.01f), _layer5a.getOptimizer()),
+    _layer6a(0.01f, ActivationReLU(0.01f)), _layer6b(ActivationReLU(0.01f), _layer6a.getOptimizer()),
+    _layer7a(0.01f, ActivationReLU(0.01f)), _layer7b(ActivationReLU(0.01f), _layer7a.getOptimizer()),
+    _layer8a(0.01f, ActivationReLU(0.01f)), _layer8b(ActivationReLU(0.01f), _layer8a.getOptimizer()),
+    _layer9a(0.01f, ActivationReLU(0.01f)), _layer9b(ActivationReLU(0.01f), _layer9a.getOptimizer()),
+    _layer10a(0.01f, ActivationTanh()), _layer10b(ActivationTanh(), _layer10a.getOptimizer()),
+    _layer11(0.01f)
+{
+}
+
+double FeatureDetector::trainBatch(const TrainingBatch& batch)
+{
+    int n=0;
+    double loss = 0.0;
+    for (auto* entry : batch) {
+        loss += trainingPass(entry->f1, entry->f2, entry->similarity);
+        ++n;
+    }
+    loss /= n;
+
+    _layer1a.getOptimizer()->applyGradients<float>();
+    _layer2a.getOptimizer()->applyGradients<float>();
+    _layer3a.getOptimizer()->applyGradients<float>();
+    _layer4a.getOptimizer()->applyGradients<float>();
+    _layer5a.getOptimizer()->applyGradients<float>();
+    _layer6a.getOptimizer()->applyGradients<float>();
+    _layer7a.getOptimizer()->applyGradients<float>();
+    _layer8a.getOptimizer()->applyGradients<float>();
+    _layer9a.getOptimizer()->applyGradients<float>();
+    _layer10a.getOptimizer()->applyGradients<float>();
+    _layer11.getOptimizer()->applyGradients<float>();
+    // no need to call for b-layers since the weights are shared
+
+    return loss;
+}
+
+float FeatureDetector::trainingPass(const Feature& f1, const Feature& f2, float targetSimilarity)
+{
+    // forward propagate
+    auto v1 = _layer10a(_layer9a(_layer8a(_layer7a(_layer6a(_layer5a(_layer4a(_layer3a(_layer2a(_layer1a(f1.polar))))))))));
+    auto v2 = _layer10b(_layer9b(_layer8b(_layer7b(_layer6b(_layer5b(_layer4b(_layer3b(_layer2b(_layer1b(f2.polar))))))))));
+    auto v3 = v1-v2;
+    float similarityPrediction = _layer11(v3)(0,0);
+
+    // loss
+    float loss = similarityPrediction - targetSimilarity;
+    loss *= loss;
+
+    // backpropagate
+    Layer11::Output g;
+    g << similarityPrediction - targetSimilarity; // initial loss gradient
+    auto l11g = _layer11.backpropagate(g); // gradient after the layer 11
+
+    _layer1a.backpropagate(
+    _layer2a.backpropagate(
+    _layer3a.backpropagate(
+    _layer4a.backpropagate(
+    _layer5a.backpropagate(
+    _layer6a.backpropagate(
+    _layer7a.backpropagate(
+    _layer8a.backpropagate(
+    _layer9a.backpropagate(
+    _layer10a.backpropagate(l11g))))))))));
+
+    _layer1b.backpropagate(
+    _layer2b.backpropagate(
+    _layer3b.backpropagate(
+    _layer4b.backpropagate(
+    _layer5b.backpropagate(
+    _layer6b.backpropagate(
+    _layer7b.backpropagate(
+    _layer8b.backpropagate(
+    _layer9b.backpropagate(
+    _layer10b.backpropagate(-l11g)))))))))); // apply negative gradient to b branch (negated to form v3)
+
+    return loss;
+}
