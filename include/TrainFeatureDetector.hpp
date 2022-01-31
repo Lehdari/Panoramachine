af00840e8a2b2f5cd8bde4e8cd4037b9c7d43178
Lehdari <miika.lehtimaki@gmail.com>
2022-01-31 23:33:32 +0200

    TrainFeatureDetector: Remove unused code

--- include/TrainFeatureDetector.hpp
+++ include/TrainFeatureDetector.hpp
@@ -12,49 +12,6 @@
 #define PANORAMACHINE_TRAINFEATUREDETECTOR_HPP
 
 
-#include <vector>
-#include <random>
-
-#include "DistortImage.hpp"
-#include "Feature.hpp"
-#include "Image.hpp"
-
-
-// Original image with multiple disroted ones, used for training entry generation
-struct TrainingImage {
-    Image<Vec3f>                original;
-    std::vector<DistortedImage> distorted;
-
-    void create(Image<Vec3f>&& image,
-        int nDistorted,
-        const DistortSettings& minSettings,
-        const DistortSettings& maxSettings);
-
-    void write(const std::string& stem) const;
-    bool read(const std::string& stem, int nDistorted);
-
-private:
-    static std::default_random_engine rnd;
-};
-
-using TrainingImages = std::vector<TrainingImage>;
-
-
-struct TrainingEntry {
-    Feature f1;
-    Feature f2;
-    Vec3f   label;  // pos diff in x/y (divided by feature max radius * f1 scale), scale diff (f2 scale / f1 scale)
-
-    void writeToFile(const std::string& filename) const;
-    void readFromFile(const std::string& filename);
-};
-
-using TrainingData = std::vector<TrainingEntry>;
-using TrainingBatch = std::vector<const TrainingEntry*>;
-
-
-TrainingEntry makeTrainingEntry(const TrainingImages& trainingImages, float similarity=0.0f);
-
 void trainFeatureDetector();
 
 
